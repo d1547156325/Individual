d8f77dce26f298def71c917fafa8b45bfcaabc96
d1547156325 <1547156325@qq.com>
2019-09-13 12:08:42 +0800

    第零步1.0  完成统计字母出现的频率

---
+++ src/main/java/Util/LetterDo.java
@@ -0,0 +1,97 @@
+package Util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class LetterDo{
+
+    public static void letters(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        List<Letter> letterList = new ArrayList<Letter>();
+        for(int i = 0; i<26; i++){
+            char a = (char) ('A' + i);
+            Letter letter = new Letter();
+            letter.setLetter(a);
+            letter.setCounts(0);
+            letterList.add(letter);
+        }
+        List<String> lists = IODemoByNIO.readFileByChannel(filename);
+        int count = 0;
+        for (String str : lists) {
+            for(int i = 0; i<str.length(); i++){
+                String s = str.substring(i, i+1).toUpperCase();
+                if(s.matches("[A-Z]")){
+                    char c = s.charAt(0);
+                    int j = c - 'A';
+                    //System.out.println(c);
+                    count++;
+                    int k = letterList.get(j).getCounts();
+                    k++;
+                    letterList.get(j).setCounts(k);
+                }
+            }
+        }
+        //System.out.println(count);
+        Collections.sort(letterList);
+        for(int i = 0; i<letterList.size(); i++){
+            Letter letter = letterList.get(i);
+            double m = (double) letter.getCounts() / (double) count;
+            letter.setRate(m);
+            //BigDecimal bg = new BigDecimal(m);
+            //double f1 = bg.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
+            System.out.println("字母:" + letter.getLetter() +
+                    "  个数: " + letter.getCounts() +
+                    "  百分比: " +  String .format("%.2f",m*100) +"%");
+        }
+    }
+
+    public static class Letter implements Comparable{
+        private char letter;
+        private int counts;
+        private double rate;
+
+        public char getLetter() {
+            return letter;
+        }
+
+        public void setLetter(char letter) {
+            this.letter = letter;
+        }
+
+        public int getCounts() {
+            return counts;
+        }
+
+        public void setCounts(int counts) {
+            this.counts = counts;
+        }
+
+        public double getRate() {
+            return rate;
+        }
+
+        public void setRate(double rate) {
+            this.rate = rate;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+
+            if(o instanceof Letter){
+                Letter letter = (Letter) o;
+                if(this.counts < ((Letter) o).counts)
+                    return 1;
+                else if(this.counts == ((Letter) o).counts)
+                    return 0;
+                else
+                    return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,16 @@
+package domain;
+
+import Util.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String args[] ) throws FileNotFoundException {
+
+        LetterDo.letters("D:\\course\\软件工程\\GameOfThron.txt");
+        //LetterDo.letters("D:\\123.txt");
+    }
+
+}
