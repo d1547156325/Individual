01a0729b840de7f186060cd11ac0e27b6ec201d9
d1547156325 <1547156325@qq.com>
2019-09-17 14:22:32 +0800

    第一步1.2  完成第一步功能2，3，实现遍历文件夹里的文件输出单词个数

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -8,9 +8,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String args[] ) throws FileNotFoundException {
+        //Long staet=System.currentTimeMillis();
+        //String fileName = "D:\\\\course\\\\软件工程\\ddffg.txt";
+        String fileName = "D:\\course\\软件工程";
+        //String fileName = "D:\\course\\软件工程\\Love in the Time of Cholera\u0083\u0085.txt";
+        //WordsDo.outWords(fileName);
 
-        LetterDo.letters("D:\\course\\软件工程\\GameOfThron.txt");
-        //LetterDo.letters("D:\\123.txt");
+        //WordsDo.directoryTxt(fileName);
+
+        //WordsDo.directory(fileName);
+
+        //WordsDoN.directoryTxt(fileName, 3);
+
+        WordsDoN.directory(fileName, 3);
+
+       // LetterDo.letters(fileName);
     }
 
 }
