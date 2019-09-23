c74b474d455d0b881783bd2d634d8c00785b88f6
s1547156325 <1547156325@qq.com>
2019-09-23 10:26:50 +0800

    3.0 完成后面几步所有功能，并进行测试

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -11,12 +11,16 @@ public class Main {
 
         String stopwords = "D:\\course\\软件工程\\stopwords.txt";
         //Long staet=System.currentTimeMillis();
-        //String fileName = "D:\\\\course\\\\软件工程\\ddffg.txt";
+        //String fileName = "D:\\course\\\\软件工程\\ddffg.txt";
         String fileName = "D:\\course\\软件工程";
         //String fileName = "D:\\course\\软件工程\\Love in the Time of Cholera\u0083\u0085.txt";
         //WordsDo.outWords(stopwords, fileName, -1);
 
-        WordsDo.directoryTxt(stopwords, fileName, -1);
+        WordsDo.directoryTxt(stopwords, fileName, 5);
+        //WordsDoN.directoryTxt(stopwords, fileName, 10);
+
+        //PhraseDo.outPhrase(fileName, 2);
+       // PhraseDo.verbDo(fileName);
 
         //WordsDo.directory(fileName);
 
